a4f399664730912fd884b0a6ddf469f7854fb7b4
Dashrath Choudhary <choudharydasrath@outlook.com>
2025-09-30 20:37:19 +0530

    gave functionality to about button

--- app/src/main/java/com/example/picksick/about.java
+++ app/src/main/java/com/example/picksick/about.java
@@ -1,6 +1,9 @@
 package com.example.picksick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class about extends AppCompatActivity {
+    ImageView imageView;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,20 @@ public class about extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imageView=findViewById(R.id.imageView);
+        result=findViewById(R.id.result);
+        result.setText("This is a offline single player game , where you have to answer varios questions about your favorite web series .....\n ALL THE BEST!!!!!!!!!!!!!!!!!!!\n  Do well and don't forget to check your score");
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
+
+
     }
 }
