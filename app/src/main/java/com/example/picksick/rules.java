04502b36f2eca02540e36f4bdd3224da735d2a81
Dashrath Choudhary <choudharydasrath@outlook.com>
2025-10-01 15:10:39 +0530

    adding functionality to rules button

--- app/src/main/java/com/example/picksick/rules.java
+++ app/src/main/java/com/example/picksick/rules.java
@@ -2,6 +2,9 @@
 package com.example.picksick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class rules extends AppCompatActivity {
-
+    TextView rresult;
+    ImageButton imbt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,15 @@ public class rules extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        rresult=findViewById(R.id.rresult);
+        imbt=findViewById(R.id.imageButton);
+        rresult.setText("1.You cannot selectmore than one option\n2.you cannot go back after submiting the answer\n3.Do not disclose the answers to others\n4.you can take your time to answer but take wisely !!\nall the best !!!!");
+        imbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
